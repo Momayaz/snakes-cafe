0d920e4c6ee8c7e559c457f6c86e1adbf3dfa60e
Momayaz <momayaz.dr.1@gmail.com>
2020-11-30 13:50:18 +0200

    modifying

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -8,27 +8,7 @@ def basics():
  print("** To quit at any time, type quit **")
  print("**************************************")
  
- print("Appetizers")
- for i in Appetizers:
-    print(i)
-
- print("Entrees")
- print("-------")
-
- for i in Entrees:
-    print(i)
-
- print("Desserts")
- print("-------")
-
  
- for i in Desserts:
-    print(i)
-
- print("Drinks")
- print("-------")
- for i in Drinks:
-    print(i)
 
 
  i = 1
@@ -38,7 +18,7 @@ def basics():
      print('***********************************')
 
      print(f">{order}")
-     print(f"** {i} order of Wings have been added to your meal **")
+     print(f"** {i} order of {order} have been added to your meal **")
 
      i+=1
      reply = input("If you want to exit, just type quit!")
